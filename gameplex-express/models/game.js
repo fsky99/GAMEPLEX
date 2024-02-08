5c9d83ea34fedb71dd7421d89300c66049337878
fatima <fatimafouad9999@gmail.com>
2024-02-08 12:44:49 +0300

    created the games schema

---
+++ gameplex-express/models/game.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const gamesSchema = new Schema({
+  content: {
+    name: String,
+    type: String,
+    poster: String,
+    sessionIds: [{ type: Schema.Types.ObjectId }],
+  },
+  timestamp: true,
+})
+
+module.exports = mongoose.model('Games', gamesSchema)
