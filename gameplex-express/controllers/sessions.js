0adebb9af18f6a64d125acd392d4aa34f059de56
9Jassim <jassim9.a@gmail.com>
2024-02-11 09:02:38 +0300

    add created session id to game

--- gameplex-express/controllers/sessions.js
+++ gameplex-express/controllers/sessions.js
@@ -1,16 +1,20 @@
 const Session = require('../models/session')
+const Game = require('../models/game')
 
 const createSession = async (req, res) => {
   try {
     const session = new Session({
-      PlayerIds: [req.body.user],
+      PlayerIds: [req.user._id],
       loacation: req.body.location,
       date: req.body.date,
       gameId: req.params.id
     })
     await session.save()
+    const game = await Game.findById(req.params.id)
+    game.sessionIds.push(session._id)
+    await game.save()
 
-    res.redirect(`/games/${req.params.id}`)
+    await res.redirect(`/games/${req.params.id}`)
   } catch (err) {
     console.log(err)
     res.redirect(`/games/${req.params.id}`)
