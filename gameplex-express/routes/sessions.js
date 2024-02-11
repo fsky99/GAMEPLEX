bd5534157f12a760b40dc3a1433eb5d948695e70
9Jassim <jassim9.a@gmail.com>
2024-02-11 10:15:02 +0300

    create leave session route

--- gameplex-express/routes/sessions.js
+++ gameplex-express/routes/sessions.js
@@ -4,5 +4,6 @@ const sessionsCtrl = require('../controllers/sessions')
 
 router.post('/games/:id/sessions', sessionsCtrl.create)
 router.put('/sessions/:id/join', sessionsCtrl.join)
+router.put('/sessions/:id/leave', sessionsCtrl.leave)
 
 module.exports = router
