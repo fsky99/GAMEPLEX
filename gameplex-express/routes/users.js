2d3c23b58b78daffaeaa869489156fee27673825
fatima <fatimafouad9999@gmail.com>
2024-02-11 14:54:32 +0300

    profile page

--- gameplex-express/routes/users.js
+++ gameplex-express/routes/users.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.get('/profile', function(req, res, next) {
-  res.render("games/profile", { title: "Profile " })});
-
-module.exports = router;
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource")
+})
+router.get("/profile", function (req, res, next) {
+  // res.render("games/profile", { title: "Profile " },user)});
+  let user = req.user
+  res.render("/games/profile", user)
+})
+module.exports = router
