12a440f8db6c6dc97bd22565537e0f83d66d58b5
9Jassim <jassim9.a@gmail.com>
2024-02-11 10:10:45 +0300

    update join session route

--- gameplex-express/routes/sessions.js
+++ gameplex-express/routes/sessions.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const sessionsCtrl = require('../controllers/sessions')
 
 router.post('/games/:id/sessions', sessionsCtrl.create)
-router.put('/games/:id/join', sessionsCtrl.join)
+router.put('/sessions/:id/join', sessionsCtrl.join)
 
 module.exports = router
