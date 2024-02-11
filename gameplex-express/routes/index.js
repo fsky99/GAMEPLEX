7b38855a10ffd5735bd9896ab5b34f906c18774d
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-02-11 10:56:04 +0300

    Edited index.js

--- gameplex-express/routes/index.js
+++ gameplex-express/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
+const passport = require("passport")
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('/games');
-});
+router.get("/", (req, res) => {
+  res.render("/games")
+})
 
-module.exports = router;
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+)
+
+module.exports = router
