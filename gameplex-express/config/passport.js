cdf86f4a38d617c9469e138d70c20aa24fdba7a3
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-02-11 10:48:03 +0300

    Edited passport.js file

--- gameplex-express/config/passport.js
+++ gameplex-express/config/passport.js
@@ -1,2 +1,29 @@
 const passport = require("passport")
 const User = require("./models/user")
+
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      try {
+        let user = await User.findOne({ googleId: profile.id })
+        if (user) return cb(null, user)
+        user = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
+        })
+        return cb(null, user)
+      } catch (err) {
+        return cb(err)
+      }
+    }
+  )
+)
