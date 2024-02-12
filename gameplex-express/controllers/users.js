2501d6f7396e4841bd35dafa82f986e5bfadda4c
9Jassim <jassim9.a@gmail.com>
2024-02-12 13:13:25 +0300

    update show function to populate users

--- gameplex-express/controllers/users.js
+++ gameplex-express/controllers/users.js
@@ -3,6 +3,10 @@ const User = require('../models/user')
 const show = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).populate('sessionsId')
+    for (let i = 0; i < user.sessionsId.length; i++) {
+      await user.sessionsId[i].populate('playersIds')
+    }
+    user.save()
     res.render('users/profile', { title: user.name, user })
   } catch (error) {
     console.log(error)
