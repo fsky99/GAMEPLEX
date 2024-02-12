5d297f52f589e45be62a10e67b5bdc3b71e23174
Shaikha <shaikha.s.ebrahim@gmail.com>
2024-02-12 15:48:33 +0300

    Editing style.css

--- gameplex-express/controllers/games.js
+++ gameplex-express/controllers/games.js
@@ -1,30 +1,28 @@
-const Game = require('../models/game')
+const Game = require("../models/game")
 
 module.exports = {
   index,
   show,
   new: newGame,
-  create
+  create,
 }
 
 async function index(req, res) {
   const games = await Game.find({})
-  res.render('games/index', { title: 'All Gmaes', games })
+  res.render("games/index", { title: "All Games", games })
 }
 
 async function show(req, res) {
-  const game = await Game.findById(req.params.id).populate('sessionIds')
+  const game = await Game.findById(req.params.id).populate("sessionIds")
   for (let i = 0; i < game.sessionIds.length; i++) {
-    await game.sessionIds[i].populate('playersIds')
+    await game.sessionIds[i].populate("playersIds")
   }
   await game.save()
-  res.render('games/show', { title: 'Game Detail', game })
+  res.render("games/show", { title: "Game Detail", game })
 }
 
 function newGame(req, res) {
-
   res.render("games/new", { title: "Add New Game", errorMsg: "" })
-
 }
 async function create(req, res) {
   console.log(req.body)
@@ -34,9 +32,9 @@ async function create(req, res) {
   try {
     // req.body.sessionIds = []
     await Game.create(req.body)
-    res.redirect('/games')
+    res.redirect("/games")
   } catch (err) {
-    console.log('This is the error!!!' + err)
-    res.render('games/new', { errorMsg: err.message })
+    console.log("This is the error!!!" + err)
+    res.render("games/new", { errorMsg: err.message })
   }
 }
